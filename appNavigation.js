38294de5f62370d74824d4eba95c80a341e7e6bb
LeonidGoldberg <leonid.goldberg@outlook.com>
2023-07-31 18:42:26 +0300

    Add icons to the app's navigation tabs

--- appNavigation.js
+++ appNavigation.js
@@ -1,16 +1,37 @@
 // https://reactnavigation.org/docs/bottom-tab-navigator
+// https://www.npmjs.com/package/react-native-ionicons
+// https://reactnavigation.org/docs/tab-based-navigation
+// https://www.npmjs.com/package/react-native-vector-icons
 
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import TriggersScreen from "./screens/TriggersScreen";
 import HabitsScreen from "./screens/HabitsScreen";
 import DashboardScreen from "./screens/DashboardScreen";
 import SettingsScreen from "./screens/SettingsScreen";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 const Tab = createBottomTabNavigator();
 
 function AppTabs() {
   return (
-    <Tab.Navigator id="appTabNavigator" initialRouteName="Habits">
+    <Tab.Navigator
+      id="appTabNavigator"
+      initialRouteName="Habits"
+      screenOptions={({ route }) => ({
+        //  based on the example in https://reactnavigation.org/docs/tab-based-navigation
+        tabBarIcon: () => {
+          if (route.name === "Habits") {
+            return <Ionicons name={"barbell-outline"} size={20} />;
+          } else if (route.name === "Triggers") {
+            return <Ionicons name={"alarm-outline"} size={20} />;
+          } else if (route.name === "Dashboard") {
+            return <Ionicons name={"grid-outline"} size={20} />;
+          } else if (route.name === "Settings") {
+            return <Ionicons name={"settings-outline"} size={20} />;
+          }
+        },
+      })}
+    >
       <Tab.Screen name="Triggers" component={TriggersScreen} />
       <Tab.Screen name="Habits" component={HabitsScreen} />
       <Tab.Screen name="Dashboard" component={DashboardScreen} />
