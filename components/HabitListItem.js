38c72fbb45bf3c02a21cd5318568b221bbf38a43
LeonidGoldberg <leonid.goldberg@outlook.com>
2023-08-04 12:04:41 +0300

    add a habit badge for whether habit is formed

--- components/HabitListItem.js
+++ components/HabitListItem.js
@@ -3,10 +3,20 @@ import {Text} from 'native-base'
 import { NativeBaseProvider, Box, Stack, IconButton, Icon } from "native-base";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import ContentBox from "./ContentBox";
+import { Badge } from 'native-base';
 
 export default function HabitListItem({ habit }) {
+    let badge;
+    if (!habit.isFormed) {
+        badge = <Badge variant="solid" borderRadius={10} colorScheme="info">Building</Badge>
+    } else {
+        badge = <Badge variant="solid" borderRadius={10} colorScheme="success">Formed</Badge>
+    }
     return (
         <ContentBox>
-            <Text>Habit</Text>
+            <Stack direction="column" alignItems="left">
+                <Text fontSize="xl">{habit.name}</Text>
+                {badge}
+            </Stack>
         </ContentBox>
     )}
