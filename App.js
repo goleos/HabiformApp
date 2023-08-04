1555ed75da396f1a46c1b69c11fbda4fe30114c5
LeonidGoldberg <leonid.goldberg@outlook.com>
2023-08-04 11:40:31 +0300

    wrap app in nativeBaseProvider

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import AppTabs from "./appNavigation";
 import { NavigationContainer } from "@react-navigation/native";
+import {NativeBaseProvider} from "native-base";
 
 export default function App() {
   return (
+      <NativeBaseProvider>
     <NavigationContainer>
       <AppTabs />
     </NavigationContainer>
+      </NativeBaseProvider>
   );
 }
