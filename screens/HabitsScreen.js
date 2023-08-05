9e4f32f8a0bd0e6ecbb40719d985ec4a68425864
LeonidGoldberg <leonid.goldberg@outlook.com>
2023-08-05 23:33:25 +0300

    add a FAB button to add a new trigger or habit

--- screens/HabitsScreen.js
+++ screens/HabitsScreen.js
@@ -1,41 +1,38 @@
 import { StyleSheet, Text, View } from "react-native";
 import { hab } from "../models/habit";
-import { Icon, IconButton, ScrollView, Stack } from "native-base";
+import { Fab, Icon, IconButton, ScrollView, Stack } from "native-base";
 import HabitListItem from "../components/HabitListItem";
 import { habitsController } from "../controllers/habitsController";
 import Ionicons from "react-native-vector-icons/Ionicons";
 
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import AddHabitScreen from "./AddHabitScreen";
 
 const NavStack = createNativeStackNavigator();
 
-export default function HabitsScreen({ navigation }) {
+export default function HabitsScreen({ navigation, isFocused }) {
   return (
-      <>
-          <NavStack.Navigator>
-              <NavStack.Screen name="NewHabit" component={AddHabitScreen} />
-          </NavStack.Navigator>
-          <Stack direction="column" padding={1}>
-              <ScrollView>
-                  {habitsController.habits.map((habit) => (
-                      <HabitListItem habit={habit} key={habit.habitID} />
-                  ))}
-                  <IconButton
-                      icon={
-                          <Icon
-                              as={Ionicons}
-                              name="add"
-                              onPress={() => {
-                                  navigation.navigate("AddNewHabit");
-                              }}
-                          />
-                      }
-                  />
-              </ScrollView>
-          </Stack>
-      </>
-
+    <>
+      <Stack direction="column" padding={1}>
+        {/* https://docs.nativebase.io/fab */}
+        <Fab
+          renderInPortal={false}
+          marginBottom={0}
+          placement="bottom-right"
+          colorScheme="blue"
+          size="lg"
+          icon={<Icon name="add" as={Ionicons} />}
+          onPress={() => {
+            navigation.navigate("New Habit");
+          }}
+        />
+        <ScrollView>
+          {habitsController.habits.map((habit) => (
+            <HabitListItem habit={habit} key={habit.habitID} />
+          ))}
+        </ScrollView>
+      </Stack>
+    </>
   );
 }
 
--- screens/TriggersScreen.js
+++ screens/TriggersScreen.js
@@ -1,15 +1,27 @@
 import { StyleSheet } from "react-native";
 import TriggerListItem from "../components/TriggerListItem";
 import { trig } from "../models/trigger";
-import { Stack } from "native-base";
+import {Fab, Icon, Stack} from "native-base";
 import { NativeBaseProvider } from "native-base/src/core/NativeBaseProvider";
 import {Text} from "native-base";
 import {View} from "native-base"
 import ContentBox from '../components/ContentBox'
+import Ionicons from "react-native-vector-icons/Ionicons";
 
-export default function TriggersScreen() {
+export default function TriggersScreen({navigation}) {
   return (
 <View backgroundColor="white">
+    <Fab
+        renderInPortal={false}
+        marginBottom={0}
+        placement="bottom-right"
+        colorScheme="blue"
+        size="lg"
+        icon={<Icon name="add" as={Ionicons} />}
+        onPress={() => {
+            navigation.navigate("Add trigger");
+        }}
+    />
     <Text alignSelf="center" padding={3}>
         Triggers are actions that you already do regularly. Linking a desired habit to a behaviour you already do inreases your chance of regularly repeating your habit.
     </Text>
