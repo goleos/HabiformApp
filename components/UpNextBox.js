5f722d198354355282530312b6fe72aa73b2ca9c
LeonidGoldberg <leonid.goldberg@outlook.com>
2023-08-22 11:06:36 +0100

    FR 4.3 complete: Implementation intentions are displayed in the dashboard

--- components/UpNextBox.js
+++ components/UpNextBox.js
@@ -1,23 +1,58 @@
-import { Box, Heading, HStack, Text } from "native-base";
+import {
+  Box,
+  Divider,
+  Heading,
+  HStack,
+  ScrollView,
+  Select,
+  Text,
+  VStack,
+} from "native-base";
 import TriggerListItem from "./TriggerListItem";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import HabitList from "./HabitList";
+import { useState } from "react";
+import IntentionsList from "./IntentionsList";
 
 export default function UpNextBox({ trigger, habits }) {
+  const hasHabits = habits.length >= 1;
+  const [selectedHabit, setSelectedHabit] = useState(
+    hasHabits ? habits[0] : null
+  );
+
   return (
-    <Box borderWidth={3} borderRadius={10} padding={2}>
-      <HStack justifyContent={"center"} alignItems={"center"} space={2}>
-        <MaterialCommunityIcons name={"clock-outline"} size={40} />
-        <Text highlight fontSize={"2xl"}>
-          {trigger.timeIntervalStart + "—" + trigger.timeIntervalEnd}
-        </Text>
+    <Box borderWidth={2} borderRadius={10} padding={2}>
+      <HStack
+        id="triggerInfo"
+        justifyContent={"center"}
+        alignItems={"center"}
+        space={2}
+      >
+        <MaterialCommunityIcons name={"clock-outline"} size={50} />
+        <VStack space={0}>
+          <Text>{trigger.name}</Text>
+          <Text highlight fontSize={"lg"}>
+            {trigger.timeIntervalStart + " — " + trigger.timeIntervalEnd}
+          </Text>
+        </VStack>
       </HStack>
-      <TriggerListItem trigger={trigger} />
+      <Divider my={2} />
       <Heading>Habits</Heading>
-      {habits !== [] ? (
-        <HabitList habits={habits} />
-      ) : (
-        <Text>No habits are linked</Text>
+      {hasHabits && (
+          <VStack space={1} padding={2}>
+
+        <Select
+          selectedValue={selectedHabit}
+          onValueChange={(itemValue) => {
+            setSelectedHabit(itemValue);
+          }}
+        >
+          {habits.map((habit) => (
+            <Select.Item label={habit.name} value={habit} key={habit.habitID} />
+          ))}
+        </Select>
+            <IntentionsList intentions={selectedHabit.intentions} readOnly={true} />
+          </VStack>
       )}
     </Box>
   );
