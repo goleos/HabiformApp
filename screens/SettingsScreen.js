eb7a3eeb630d57a12d6e10b96295fe9e59f5afa5
LeonidGoldberg <leonid.goldberg@outlook.com>
2023-08-28 16:26:59 +0100

    Let user delete their data

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,17 +1,33 @@
 import { SafeAreaView, Switch } from "react-native";
-import { Button, Flex, Heading, HStack, Text, VStack } from "native-base";
+import {AlertDialog, Button, Flex, Heading, HStack, Text, VStack} from "native-base";
 import { appSettingsController } from "../controllers/AppSettingsController";
 import { observer } from "mobx-react";
 import BoxStack from "../components/boxes/BoxStack";
 import InfoAlert from "../components/InfoAlert";
+import { dbController } from "../controllers/DatabaseController";
+import { habitsController } from "../controllers/HabitsController";
+import { triggersController } from "../controllers/TriggersController";
+import {useRef, useState} from "react";
 
 function SettingsScreen() {
+    const [isOpenDeleteModal, setIsOpenDeleteModal] = useState(false)
+
+    const emptyRef = useRef(null)
   return (
     <SafeAreaView>
-      <Flex bg={"white"} height={"100%"} padding={2} justifyContent={'space-between'}>
+      <Flex
+        bg={"white"}
+        height={"100%"}
+        padding={2}
+        justifyContent={"space-between"}
+      >
         <VStack space={4}>
-          <BoxStack>
-            <InfoAlert text={'Turning this setting off means that you will not receive any information about implementation intentions, when you are reminded about a habit'} />
+          <BoxStack title={'Notification settings'}>
+            <InfoAlert
+              text={
+                "Turning this setting off means that you will not receive any information about implementation intentions, when you are reminded about a habit"
+              }
+            />
             <HStack justifyContent={"space-between"} alignItems={"center"}>
               <Text fontSize={"md"}>Remind with habit intentions</Text>
               <Switch
@@ -23,21 +39,32 @@ function SettingsScreen() {
               />
             </HStack>
           </BoxStack>
+          <BoxStack title={"Data management"}>
+            <Button
+                colorScheme={'delete'}
+              onPress={() => {
+                dbController.deleteEverything();
+                habitsController.requestHabits();
+                triggersController.requestTriggers();
+              }}
+            >
+              Delete all data
+            </Button>
+          </BoxStack>
           <BoxStack>
-            <HStack paddingY={2} justifyContent={'space-between'}>
+            <HStack paddingY={2} justifyContent={"space-between"}>
               <Text>App version</Text>
               <Text mr={3}>0.0.1</Text>
             </HStack>
           </BoxStack>
         </VStack>
-          <Button
-            onPress={async () => {
-              await appSettingsController.resetSettings();
-            }}
-          >
-            Reset settings
-          </Button>
-
+        <Button
+          onPress={async () => {
+            await appSettingsController.resetSettings();
+          }}
+        >
+          Reset settings
+        </Button>
       </Flex>
     </SafeAreaView>
   );
