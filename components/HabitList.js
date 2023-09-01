df044a4f7989900749acbf46ba887f7318fae7eb
LeonidGoldberg <leonid.goldberg@outlook.com>
2023-09-01 16:17:23 +0100

    Fix habits not fully showing in the scrollview

--- components/HabitList.js
+++ components/HabitList.js
@@ -1,26 +1,29 @@
-import {habitsController} from "../controllers/HabitsController";
-import {Pressable, ScrollView, Text, Stack} from "native-base";
+import { habitsController } from "../controllers/HabitsController";
+import { Pressable, ScrollView, Text, Stack } from "native-base";
 import HabitListItem from "./listItems/HabitListItem";
 
-function HabitList({habits, onItemPress, hideArrowButton}) {
-    return (
-        <Stack direction="column" padding={1}>
-            {habits !== null ? (
-                habits.map((habit) => onItemPress !== undefined ? (
-                    <Pressable
-                        key={habit.habitID}
-                        onPress={() => onItemPress(habit)}
-                    >
-                        <HabitListItem habit={habit} />
-                    </Pressable>
-                ) : (
-                    <HabitListItem hideArrowButton={hideArrowButton} key={habit.habitID} habit={habit} />
-                ))
-            ) : (
-                <Text>Loading...</Text>
-            )}
-        </Stack>
-    )
+function HabitList({ habits, onItemPress, hideArrowButton }) {
+  return (
+    <Stack direction="column" padding={1}>
+      {habits !== null ? (
+        habits.map((habit) =>
+          onItemPress !== undefined ? (
+            <Pressable key={habit.habitID} onPress={() => onItemPress(habit)}>
+              <HabitListItem habit={habit} />
+            </Pressable>
+          ) : (
+            <HabitListItem
+              hideArrowButton={hideArrowButton}
+              key={habit.habitID}
+              habit={habit}
+            />
+          )
+        )
+      ) : (
+        <Text>Loading...</Text>
+      )}
+    </Stack>
+  );
 }
 
-export default HabitList
+export default HabitList;
--- screens/habits/HabitsScreen.js
+++ screens/habits/HabitsScreen.js
@@ -1,18 +1,18 @@
-import {Button, Fab, Flex, Icon, ScrollView, Text, VStack} from "native-base";
+import { Button, Fab, Flex, Icon, ScrollView, Text, VStack } from "native-base";
 import { habitsController } from "../../controllers/HabitsController";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import { observer } from "mobx-react";
 import HabitList from "../../components/HabitList";
 import ContentBox from "../../components/ContentBox";
-import {useState} from "react";
+import { useState } from "react";
 import HabitStatus from "../../models/habitStatus";
 
 function HabitsScreen({ navigation, isFocused }) {
   // console.log(habitsController.habits);
-    const [filterValue, setFilterValue] = useState(HabitStatus.Active)
-    const habits = habitsController.habits.filter((habit) => {
-        return habit.habitStatus === filterValue
-    })
+  const [filterValue, setFilterValue] = useState(HabitStatus.Active);
+  const habits = habitsController.habits.filter((habit) => {
+    return habit.habitStatus === filterValue;
+  });
   return (
     <>
       {/* https://docs.nativebase.io/fab */}
@@ -30,35 +30,50 @@ function HabitsScreen({ navigation, isFocused }) {
         }}
       />
 
-            <VStack space={2} backgroundColor={'white'}>
-        <Button.Group mt={3} alignSelf={'center'} isAttached colorScheme={'secondary'} size={'md'}>
-            <Button variant={filterValue !== HabitStatus.Draft ? 'outline' : 'solid'} onPress={() => {
-                setFilterValue(HabitStatus.Draft)
-            }}>Draft</Button>
-            <Button variant={filterValue !== HabitStatus.Active ? 'outline' : 'solid'} onPress={() => {
-                setFilterValue(HabitStatus.Active)
-            }} >Active</Button>
-            <Button variant={filterValue !== HabitStatus.Archived ? 'outline' : 'solid'} onPress={() => {
-                setFilterValue(HabitStatus.Archived)
-            }}>Archived</Button>
-
+      <VStack space={2} backgroundColor={"white"} flex={1}>
+        <Button.Group
+          mt={3}
+          alignSelf={"center"}
+          isAttached
+          colorScheme={"secondary"}
+          size={"md"}
+        >
+          <Button
+            variant={filterValue !== HabitStatus.Draft ? "outline" : "solid"}
+            onPress={() => {
+              setFilterValue(HabitStatus.Draft);
+            }}
+          >
+            Draft
+          </Button>
+          <Button
+            variant={filterValue !== HabitStatus.Active ? "outline" : "solid"}
+            onPress={() => {
+              setFilterValue(HabitStatus.Active);
+            }}
+          >
+            Active
+          </Button>
+          <Button
+            variant={filterValue !== HabitStatus.Archived ? "outline" : "solid"}
+            onPress={() => {
+              setFilterValue(HabitStatus.Archived);
+            }}
+          >
+            Archived
+          </Button>
         </Button.Group>
-                <Flex height={"100%"} bg={"white"}>
-        <ScrollView>
-
-
-        <HabitList
-          habits={habits}
-          onItemPress={(habit) => {
-            navigation.navigate("HabitPage", {
-              habit: habit,
-            });
-          }}
-        />
-
-        </ScrollView>
-    </Flex>
-            </VStack>
+          <ScrollView >
+            <HabitList
+              habits={habits}
+              onItemPress={(habit) => {
+                navigation.navigate("HabitPage", {
+                  habit: habit,
+                });
+              }}
+            />
+          </ScrollView>
+      </VStack>
     </>
   );
 }
