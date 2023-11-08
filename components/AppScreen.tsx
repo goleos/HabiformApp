c66a4b43038161b8eb07ef75e574183f87544434
goleos <goleos4@proton.me>
2023-11-08 20:04:28 +0200

    Refactor AppScreen component

--- components/AppScreen.tsx
+++ components/AppScreen.tsx
@@ -1,7 +1,30 @@
-import {View} from "native-base";
+import { View, VStack } from "native-base";
+import React from "react";
 
-export default function AppScreen(props) {
-    return <View flex={1} backgroundColor={"green.50"}>
-        {props.children}
-    </View>
-}
+type appScreenProps = {
+  spaceBetween?: boolean;
+  padding?: number;
+  children: any;
+} & typeof defaultProps;
+
+const defaultProps = {
+  spaceBetween: true,
+  padding: 0,
+};
+
+// https://react-typescript-cheatsheet.netlify.app/docs/basic/getting-started/default_props/
+const AppScreen = (props: appScreenProps) => {
+  return (
+    <VStack
+      flex={1}
+      backgroundColor={"#cfd3f6"}
+      justifyContent={(props.spaceBetween) ? "space-between" : undefined}
+      padding={props.padding}
+    >
+      {props.children}
+    </VStack>
+  );
+};
+AppScreen.defaultProps = defaultProps;
+
+export default AppScreen;
