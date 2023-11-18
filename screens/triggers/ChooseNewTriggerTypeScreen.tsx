6276a0aa238751c868d49c3a323b5d54681f7859
goleos <goleos4@proton.me>
2023-11-18 20:55:14 +0200

    Create a dedicated screen for adding a trigger, and choosing trigger type

---
+++ screens/triggers/ChooseNewTriggerTypeScreen.tsx
@@ -0,0 +1,10 @@
+import {View, Text} from "native-base";
+
+
+export default function ChooseNewTriggerTypeScreen() {
+    return (
+        <View>
+        <Text>ChooseNewTriggerTypeScreen</Text>
+        </View>
+    );
+}
