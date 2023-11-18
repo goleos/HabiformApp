2a970a58d7d057c61849773ef4d74c97583bc6af
goleos <goleos4@proton.me>
2023-11-18 17:38:43 +0200

    Add pressed effect for content boxes

--- components/ContentBox.js
+++ components/ContentBox.js
@@ -1,32 +1,42 @@
-import { Box, Icon, Stack } from "native-base";
+import {Box, Icon, Pressable, Stack} from "native-base";
 import Ionicons from "react-native-vector-icons/Ionicons";
 
 export default function ContentBox(props) {
   return (
-    <Box
-      backgroundColor="white"
-      borderRadius={15}
-      paddingLeft={3}
-      paddingY={3}
-      margin={2}
-      shadow={6}
-    >
-      <Stack
-        direction="row"
-        alignItems="center"
-        space={5}
-        justifyContent="space-between"
-      >
-        {props.children}
-        {props.hideArrowButton !== true && (
-          <Icon
-            margin={3}
-            size={19}
-            as={Ionicons}
-            name="chevron-forward-outline"
-          />
-        )}
-      </Stack>
-    </Box>
-  );
+      <Pressable onPress={props.onPress}>
+        {({
+          isHovered,
+          isFocused,
+          isPressed,
+        }) => {
+          return (
+              <Box
+                  backgroundColor={isPressed ? "gray.200" : "white"}
+                  borderRadius={15}
+                  paddingLeft={3}
+                  paddingY={3}
+                  margin={2}
+                  shadow={6}
+              >
+                <Stack
+                    direction="row"
+                    alignItems="center"
+                    space={5}
+                    justifyContent="space-between"
+                >
+                  {props.children}
+                  {props.hideArrowButton !== true && (
+                      <Icon
+                          margin={3}
+                          size={19}
+                          as={Ionicons}
+                          name="chevron-forward-outline"
+                      />
+                  )}
+                </Stack>
+              </Box>
+          );
+        }}
+      </Pressable>
+);
 }
--- screens/triggers/TriggersScreen.js
+++ screens/triggers/TriggersScreen.js
@@ -1,50 +1,52 @@
 import TriggerListItem from "../../components/listItems/TriggerListItem";
 import {Fab, Icon, Pressable, ScrollView, View, VStack} from "native-base";
 import Ionicons from "react-native-vector-icons/Ionicons";
-import { triggersController } from "../../controllers/TriggersController";
-import { observer } from "mobx-react";
+import {triggersController} from "../../controllers/TriggersController";
+import {observer} from "mobx-react";
 import InfoAlert from "../../components/InfoAlert";
-import { focusedTriggerController } from "../../controllers/FocusedTriggerController";
-import { triggerScheduleController } from "../../controllers/TriggerScheduleController";
+import {focusedTriggerController} from "../../controllers/FocusedTriggerController";
+import {triggerScheduleController} from "../../controllers/TriggerScheduleController";
 import {i18n} from "../../utils/localisation";
 import AppScreen from "../../components/AppScreen";
 
-function TriggersScreen({ navigation }) {
+function TriggersScreen({navigation}) {
+    const handleCreateTrigger = () => {
+        focusedTriggerController.newTrigger();
+        navigation.navigate("ManageTrigger");
+    };
 
-  const handleCreateTrigger = () => {
-    focusedTriggerController.newTrigger();
-    navigation.navigate("ManageTrigger");
-  };
+    const handleViewTrigger = (trigger) => {
+        focusedTriggerController.setTrigger(trigger);
+        navigation.navigate("Trigger");
+    };
 
-  const handleViewTrigger = (trigger) => {
-    focusedTriggerController.setTrigger(trigger);
-    navigation.navigate("Trigger");
-  };
-
-  return (
-    <AppScreen>
-      <InfoAlert heading={i18n.t("tipAlertBoxName")} text={i18n.t("triggersScreenTip")} />
-      <ScrollView>
-        {triggersController.triggers.map((trigger) => (
-          <Pressable
-            key={trigger.triggerEventID}
-            onPress={() => handleViewTrigger(trigger)}
-          >
-            <TriggerListItem navigation={navigation} trigger={trigger} />
-          </Pressable>
-        ))}
-      </ScrollView>
-      <Fab
-        renderInPortal={false}
-        marginBottom={0}
-        placement="bottom-right"
-        bgColor="triggerColour.100"
-        size="lg"
-        icon={<Icon name="add" as={Ionicons} />}
-        onPress={handleCreateTrigger}
-      />
-    </AppScreen>
-  );
+    return (
+        <AppScreen>
+            <InfoAlert
+                heading={i18n.t("tipAlertBoxName")}
+                text={i18n.t("triggersScreenTip")}
+            />
+            <ScrollView>
+                {triggersController.triggers.map((trigger) => (
+                    <TriggerListItem
+                        key={trigger.triggerEventID}
+                        onPress={() => handleViewTrigger(trigger)}
+                        navigation={navigation}
+                        trigger={trigger}
+                    />
+                ))}
+            </ScrollView>
+            <Fab
+                renderInPortal={false}
+                marginBottom={0}
+                placement="bottom-right"
+                bgColor="triggerColour.100"
+                size="lg"
+                icon={<Icon name="add" as={Ionicons}/>}
+                onPress={handleCreateTrigger}
+            />
+        </AppScreen>
+    );
 }
 
 export default observer(TriggersScreen);
